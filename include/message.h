37e62a4ca7538580ea70c6ea36fd089994092932
Romain Lefebvre <romain.lefebvre1104@gmail.com>
2023-03-11 14:55:35 +0100

    [ADD] message for enable led

--- include/message.h
+++ include/message.h
@@ -1,16 +1,25 @@
 #ifndef MESSAGE_H
 #define MESSAGE_H
 
+#include <db-utils/types.h>
+
 #define MAX_LENGTH_BARCODE 13
 
 typedef struct
 {
     char barcode[MAX_LENGTH_BARCODE];
+    float price;
 } scanner_message_t;
+
+typedef struct
+{
+    ip_address_t ip_address;
+} led_message_t;
 
 typedef union
 {
     scanner_message_t scanner;
+    led_message_t led;
 } message_t;
 
 typedef struct
