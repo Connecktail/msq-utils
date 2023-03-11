7013d29c83a01c77afa31b5025029e9881d943a0
Romain Lefebvre <romain.lefebvre1104@gmail.com>
2023-03-11 15:02:46 +0100

    [ADD] message for enable buzzer

--- include/message.h
+++ include/message.h
@@ -1,6 +1,8 @@
 #ifndef MESSAGE_H
 #define MESSAGE_H
 
+#include <db-utils/types.h>
+
 #define MAX_LENGTH_BARCODE 13
 
 typedef struct
@@ -8,9 +10,15 @@ typedef struct
     char barcode[MAX_LENGTH_BARCODE];
 } scanner_message_t;
 
+typedef struct
+{
+    ip_address_t ip_address;
+} buzzer_message_t;
+
 typedef union
 {
     scanner_message_t scanner;
+    buzzer_message_t buzzer;
 } message_t;
 
 typedef struct
